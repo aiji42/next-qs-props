e298aa7e58386a80eaf99580ba9ba8ad63813291
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-11-15 22:53:02 +0900

    chore: make passable a allow list to filter query strings

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,10 +4,17 @@ import { createQueryStringPath } from './query-string-path'
 
 type Middleware = (req: NextRequest) => undefined | NextResponse
 
-export const makeQueryStringMiddleware = (option: ParseOptions): Middleware => {
+export const makeQueryStringMiddleware = (
+  option: ParseOptions & { allowKeys?: string[] }
+): Middleware => {
   return (req: NextRequest) => {
     if (!req.nextUrl.search) return
-    const qsp = createQueryStringPath(parse(req.nextUrl.search, option))
+    const params = Object.fromEntries(
+      Object.entries(parse(req.nextUrl.search, option)).filter(([key]) =>
+        (option.allowKeys ?? []).includes(key)
+      )
+    )
+    const qsp = createQueryStringPath(params)
     return NextResponse.rewrite(
       req.nextUrl.pathname === '/'
         ? `/${qsp}`
