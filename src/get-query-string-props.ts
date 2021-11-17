90610919ee1b7f26bb8d939682ad77d528d07ce4
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-11-17 23:39:03 +0900

    chore: make the return value of getQueryStringProps generic

--- src/get-query-string-props.ts
+++ src/get-query-string-props.ts
@@ -1,13 +1,15 @@
 import { parseQueryStringPath } from './query-string-path'
 import { GetStaticPropsContext } from 'next'
 
-export const getQueryStringProps = (
+export const getQueryStringProps = <
+  T extends Record<string, unknown> = Record<string, unknown>
+>(
   ctx: GetStaticPropsContext,
   pathKey: string
-): undefined | Record<string, unknown> => {
+): undefined | T => {
   const paths = ctx.params?.[pathKey]
   if (!paths) return undefined
   if (Array.isArray(paths))
-    return paths.map((p) => parseQueryStringPath(p)).find(Boolean)
-  return parseQueryStringPath(paths)
+    return paths.map((p) => parseQueryStringPath(p)).find(Boolean) as T
+  return parseQueryStringPath(paths) as T
 }
