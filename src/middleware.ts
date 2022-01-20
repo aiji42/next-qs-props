54aac430f1afcdb4bcb72ef3d28ae57c2bf73eff
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-01-20 23:20:05 +0900

    feat: change makeMiddleware option to keys only

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,21 +2,21 @@ import { NextRequest, NextResponse, NextMiddleware } from 'next/server'
 
 export const makeMiddleware = (option: {
   activeWhen?: (request: NextRequest) => boolean
-  queries: { key: string; fill: string }[]
-  path: string
+  keys: string[]
 }): NextMiddleware => {
   return (req: NextRequest) => {
     if (!(option.activeWhen ?? (() => true))(req)) return
 
-    const replacePath = option.queries.reduce((res, { key, fill }) => {
-      if (fill.length < 1)
-        console.warn(
-          'It is dangerous to pass an empty string for `fill`; it should be a string of one or more characters.'
-        )
-      const val = req.nextUrl.searchParams.get(key)
-      return res.replace(`[${key}]`, val || fill)
-    }, option.path)
+    const params = option.keys
+      .reduce<string[]>((res, key) => {
+        const val = req.nextUrl.searchParams.get(key)
+        return val ? [...res, `${key}-${val}`] : res
+      }, [])
+      .join('/')
+    if (!params) return
 
-    return NextResponse.rewrite(replacePath)
+    return NextResponse.rewrite(
+      `${req.nextUrl.pathname}/${params}`.replace(/\/\//, '/')
+    )
   }
 }
