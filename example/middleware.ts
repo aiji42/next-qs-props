c0dd43533d5f1b63eee7b49465a8057c195d4ebf
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-07-20 01:30:14 +0900

    chore: update example

--- example/middleware.ts
+++ example/middleware.ts
@@ -1,13 +1,8 @@
 import { makeMiddleware } from 'qs-props'
-import { NextMiddleware } from 'next/server'
 
-export const middleware: NextMiddleware = (request, event) => {
-  const qsMiddleware = makeMiddleware({
-    keys: ['size', 'color']
-  })
-
-  return qsMiddleware(request, event)
-}
+export const middleware = makeMiddleware({
+  keys: ['size', 'color']
+})
 
 export const config = {
   matcher: '/with-query-string/:path*'
